b10cfdde26982092facccc3288338cefbdd8c515
RVdeKoning <robert.dekoning@ogd.nl>
2017-06-28 13:37:04 +0200

    Add project files.

---
+++ Kantar.Randstad.GPS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Kantar.Randstad.GPS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Kantar.Randstad.GPS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kantar.Randstad.GPS.MvcApplication" Language="C#" %>
